a8941a4d8419826aa5f70960c342c7429fcccf74
Dzmitry Bryt <bdv212@yandex.ru>
2021-11-11 12:16:42 +0300

    feat: add eslint rule

--- src/services/cryptoApi.ts
+++ src/services/cryptoApi.ts
@@ -2,7 +2,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { ICryptoInfo } from '../models/ICoin';
 import { IHistory } from '../models/IHistory';
-import { ICoins } from '../models/ICoins';
+// import { ICoins } from '../models/ICoins';
 import { IExchanges } from '../models/IExchanges';
 
 // const cryptoApiHeaders = {
